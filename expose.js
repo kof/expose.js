925dd0c5975173426ba984665859a59d99140847
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2011-04-15 15:30:26 +0200

    first version

---
+++ expose.js
@@ -0,0 +1,38 @@
+function expose(namespace, api) {
+    var env = {};
+    
+    if (typeof namespace !== 'string') {
+        api = namespace;
+        namespace = null;
+    }
+    
+    // the global api of any environment
+    // thanks to Nicholas C. Zakas
+    // http://www.nczonline.net/blog/2008/04/20/get-the-javascript-global/
+    env.global = (function(){
+        return this;
+    }).call(null);   
+    
+    // expose passed api as exports
+    env.exports = api || {}; 
+
+    // commonjs        
+    if (typeof module !== 'undefined' && 
+        typeof exports !== 'undefined' &&
+        module.exports) {
+        env.commonjs = true;
+        env.module = module;
+        module.exports = exports = env.exports;
+    }
+    
+    // browser only
+    if (typeof window !== 'undefined') {
+        env.browser = true;
+        // we are not in amd wrapper
+        if (!env.commonjs && namespace && env.exports) {
+            env.global[namespace] = env.exports;
+        }
+    }
+
+    return env;
+}
